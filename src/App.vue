1a0938bf9f68948d15a2164f60230083e24ae229
menegain-rose <rose.menegain@edu.univ-fcomte.fr>
2022-10-25 16:56:00 +0200

    création des pages

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <template>
-  <nav>
-    <h4 class="text-xl">
-      <Bars3Icon class="inline-block h-5 w-5 text-blue-500" />
-      menu (dans <code class="font-mono">/src/App.vue</code>)
-    </h4>
-    <ul>
-      <li>
-        <router-link class="text-red-600 underline" to="/">
-          lien vers
-          <code class="font-mono">/src/pages/index.vue</code>
-        </router-link>
-      </li>
-    </ul>
-  </nav>
+  <header class="bg-black">
+    <nav>
+      <h4 class="text-xl">
+        <Bars3Icon class="inline-block h-5 w-5 text-blue-500" />
+        menu (dans <code class="font-mono">/src/App.vue</code>)
+      </h4>
+      <ul>
+        <li>
+          <router-link class="text-red-600 underline" to="/">
+            lien vers
+            <code class="font-mono">/src/pages/index.vue</code>
+          </router-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
 
   <!-- Affiche les pages -->
   <Suspense>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ const formKitTailwind = require("@formkit/themes/tailwindcss");
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+
+
+
+
+
+
+
+
+    },
   },
   plugins: [
     require("@tailwindcss/typography"),
