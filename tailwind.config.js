8f5b0b1d43eca164e6949878855e7ea2b110cc93
menegain-rose <rose.menegain@edu.univ-fcomte.fr>
2022-10-26 08:49:43 +0200

    ajout des typos et des styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,34 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
-
-
-
-
-
-
-
+      "colorStyles": {
+        "OffWhite": "#FCFCFB",
+        "LightBeige": "#EFECE6",
+        "MediumBeige": "#CBC7BE",
+        "MediumGray": "#4F4D4A",
+        "DarkGray": "#232221"
+      },
+      "fontSize": {
+        "xs": "0.875",
+        "sm": "0.625rem",
+        "base": "1rem",
+        "lg": "1.125rem",
+        "xl": "1.375rem",
+        "xxl": "1.563",
+        "big": "1.75"
+      },
+      "fontFamily": {
+        "raleway": "Raleway",
+        "athiti": "Athiti"
+      },
+      "borderRadius": {
+        "none": "0",
+        "xs": "0.313rem",
+        "sm": "0.625rem",
+        "base": "0.938rem",
+        "lg": "1.25rem",
+        "xl": "1.563rem",
+      }
 
     },
   },
