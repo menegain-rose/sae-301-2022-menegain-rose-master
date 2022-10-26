75d631d431abf7ab396b85febc42b1ae38132eb2
menegain-rose <rose.menegain@edu.univ-fcomte.fr>
2022-10-26 14:54:26 +0200

    page authentification fini

--- src/pages/AuthentificationView.vue
+++ src/pages/AuthentificationView.vue
@@ -1,9 +1,47 @@
+<script setup lang="ts">
+// import { Bars3Icon } from "@heroicons/vue/20/solid";
+import Bouton from "@/components/bouton.vue"
+
+
+</script>
+
 <template>
-  <main>
-    <section>
-      <h1 class="text-DarkGray">
-        Authentification
-      </h1>
+  <main class="my-20 mx-20">
+    <section class="bg-MediumBeige px-10 pb-10 rounded-lg">
+      <div class=" grid grid-cols-2">
+        <div>
+          <p class="bg-LightBeige py-3 px-6 my-10 rounded-sm">
+            Email
+          </p>
+          <p class="bg-LightBeige py-3 px-6 my-10 rounded-sm">
+            Mot de passe
+          </p>
+        </div>
+        <div class="ml-10">
+          <div class="py-3 px-6 my-10 rounded-sm bg-[#3B5998] text-white flex">
+            <img src="/Images/IconFacebook.png" class="w-auto h-6 mr-6" alt="Icon Facebook">
+            <p>
+              Continuer avec Facebook
+            </p>
+          </div>
+          <div class="py-3 px-6 my-10 rounded-sm bg-white text-black flex">
+            <img src="/Images/IconGoogle.png" class="w-auto h-6 mr-6" alt="Icon Google">
+            <p>
+              Continuer avec Google
+            </p>
+          </div>
+        </div>  
+      </div>
+
+      <div class="flex justify-center">
+        <Bouton class="text-OffWhite flex items-center">
+          <img src="/Icons/IconProfil.svg" class="mr-2" alt="Icon Profil">
+          <h3>
+            Connection
+          </h3>
+        </Bouton>
+
+      </div>
     </section>
 
   </main>
