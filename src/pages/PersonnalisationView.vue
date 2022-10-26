073ec85e8a777659a8c7481fa121e49517d095ca
menegain-rose <rose.menegain@edu.univ-fcomte.fr>
2022-10-26 18:24:33 +0200

    debut personnalisation, ajout composants, et svg

--- src/pages/PersonnalisationView.vue
+++ src/pages/PersonnalisationView.vue
@@ -1,7 +1,30 @@
 <template>
     <section>
-      <h1 class="text-DarkGray">
-        Perrso=bgs
-      </h1>
+      <div>
+        <p>
+          ENCORE
+        </p>
+        <p>
+          1 1 H 3 5
+        </p>
+        <p>
+          avant la fin de la réduction à
+        </p>
+        <p>
+          -25%
+        </p>
+      </div>
+      <div>
+        <div>
+          <img src="/Montres/SVGMontre.svg" alt="">
+          <p>
+            2 150 €
+          </p>
+          <p>
+            1 612 €
+          </p>
+          <img src="/Montres/MontreSVG_Small.svg" alt="">
+        </div>
+      </div>
     </section>
   </template>
