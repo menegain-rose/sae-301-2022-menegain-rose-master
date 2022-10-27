db8a8c0c1cd6fca174bc923e725b47e7a8fa3928
menegain-rose <rose.menegain@edu.univ-fcomte.fr>
2022-10-27 13:04:15 +0200

    modèles liste

--- src/pages/VosmodelesView.vue
+++ src/pages/VosmodelesView.vue
@@ -1,12 +1,31 @@
 <script setup lang="ts">
+import { supabase } from "@/supabase";
+import Montre from "../components/Montre.vue";
 
-
+const props = defineProps<{
+    max?: number;
+}>();
+const { data: montre, error } = await supabase
+    .from("montre")
+    .select("*")
+    .limit(props.max ?? 100);
+if (error) {
+    console.log("n'a pas pu récupérer les montres", { error });
+}
 </script>
 
 <template>
-    <section>
-      <h1 class="text-DarkGray">
-        montres
-      </h1>
-    </section>
-  </template>
+  <section class="mx-40 my-20">
+    <h2 class="text-DarkGray">
+      Les montres que vous avez déjà confectionner
+    </h2>
+
+    <div class="border-black border-2 ">
+      <p v-for="m in montre" :key="m.id_montre">
+        <Montre class="w-64" v-bind="m" />
+      </p>
+    </div>
+
+  </section>
+</template>
+
